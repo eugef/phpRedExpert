b076367dc4bee3f8341f7b2759ce9ad344e8f6a9
Eugene Fidelin <eugene.fidelin@gmail.com>
2014-12-31 00:34:17 +0100

    Refactor range filter to use strict mode

--- src/Eugef/PhpRedExpert/AngularBundle/Resources/public/js/filters/range.js
+++ src/Eugef/PhpRedExpert/AngularBundle/Resources/public/js/filters/range.js
@@ -1,11 +1,23 @@
-App.filter('range', function() {
-    return function(input, min, max) {
-        min = parseInt(min, 10); 
-        max = parseInt(max, 10);
-        for (var i = min; i < max; i++) {
-            input.push(i);
+App.filter('range',
+    function () {
+        "use strict";
+
+        /**
+         * @param {Array} input
+         * @param {String|Number} min
+         * @param {String|Number} max
+         * @returns {Array}
+         */
+        function filter(input, min, max) {
+            min = parseInt(min, 10);
+            max = parseInt(max, 10);
+            for (var i = min; i < max; i++) {
+                input.push(i);
+            }
+
+            return input;
         }
-        
-        return input;
-    };
-});
+
+        return filter;
+    }
+);
