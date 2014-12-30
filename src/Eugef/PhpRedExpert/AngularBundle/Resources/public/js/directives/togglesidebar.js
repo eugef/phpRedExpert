bbecaa5bc63d467a40a0550c80e8d6933ba85745
Eugene Fidelin <eugene.fidelin@gmail.com>
2014-12-30 22:43:07 +0100

    Refactor togglesidebar directive to use strict mode

--- src/Eugef/PhpRedExpert/AngularBundle/Resources/public/js/directives/togglesidebar.js
+++ src/Eugef/PhpRedExpert/AngularBundle/Resources/public/js/directives/togglesidebar.js
@@ -1,19 +1,21 @@
-App.directive('togglesidebar', ['$window', 
-    function($window) {
+App.directive('togglesidebar', ['$window',
+    function ($window) {
+        "use strict";
+
         return {
             restrict: 'A',
-            link : function(scope, elem, attrs) {
-                elem.bind('click', function(e) {
-                    e.preventDefault();
+            link: function (scope, element, attributes) {
+                element.bind('click', function (event) {
+                    event.preventDefault();
 
-                    //If window is small enough, enable sidebar push menu
+                    // If window is small enough, enable sidebar push menu
                     if ($window.innerWidth <= 992) {
                         angular.element(document.querySelector('.row-offcanvas')).toggleClass('active');
                         angular.element(document.querySelector('.left-side')).removeClass('collapse-left');
                         angular.element(document.querySelector('.right-side')).removeClass('strech');
                         angular.element(document.querySelector('.row-offcanvas')).toggleClass('relative');
                     } else {
-                        //Else, enable content streching
+                        // Else, enable content stretching
                         angular.element(document.querySelector('.left-side')).toggleClass('collapse-left');
                         angular.element(document.querySelector('.right-side')).toggleClass('strech');
                     }
