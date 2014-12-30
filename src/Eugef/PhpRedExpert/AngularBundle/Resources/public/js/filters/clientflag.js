3a84162e4ff7c6fe427a322ba1690f26bc60b4a8
Eugene Fidelin <eugene.fidelin@gmail.com>
2014-12-31 00:26:28 +0100

    Refactor clientflag filter to use strict mode

--- src/Eugef/PhpRedExpert/AngularBundle/Resources/public/js/filters/clientflag.js
+++ src/Eugef/PhpRedExpert/AngularBundle/Resources/public/js/filters/clientflag.js
@@ -1,29 +1,42 @@
-App.filter('clientflag', function() {
-    var flags = {
-        O: 'slave in MONITOR mode',
-        S: 'normal slave server',
-        M: 'master',
-        x: 'in MULTI/EXEC context',
-        b: 'waiting in a blocking operation',
-        i: 'waiting for a VM I/O',
-        d: 'a watched keys has been modified - EXEC will fail',
-        c: 'connection to be closed after writing entire reply',
-        u: 'unblocked',
-        A: 'connection to be closed ASAP',
-        N: 'normal client'
-    };
-    
-    return function(input) {
-        if (input) {
-            result = [];
-            for(i=0; i<input.length; i++) {
-                result.push(flags[input.charAt(i)]);
+App.filter('clientflag',
+    function () {
+        "use strict";
+
+        /**
+         * @type {Object}
+         */
+        var flags = {
+            O: 'slave in MONITOR mode',
+            S: 'normal slave server',
+            M: 'master',
+            x: 'in MULTI/EXEC context',
+            b: 'waiting in a blocking operation',
+            i: 'waiting for a VM I/O',
+            d: 'a watched keys has been modified - EXEC will fail',
+            c: 'connection to be closed after writing entire reply',
+            u: 'unblocked',
+            A: 'connection to be closed ASAP',
+            N: 'normal client'
+        };
+
+        /**
+         * @param {String} input
+         * @returns {String}
+         */
+        function filter(input) {
+            if (input) {
+                var result = [];
+                for (var i = 0; i < input.length; i++) {
+                    result.push(flags[input.charAt(i)]);
+                }
+
+                return result.join();
             }
-            
-            return result.join();
+            else {
+                return '-';
+            }
         }
-        else {
-            return '-';
-        }
+
+        return filter;
     }
-});
+);
